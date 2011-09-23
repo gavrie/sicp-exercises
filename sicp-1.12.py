bb70335602c2dea30de230348f5712912ec0e42f
Gavrie Philipson <gavriep@il.ibm.com>
2011-09-23 13:53:15 +0300

    functional version

--- sicp-1.12.py
+++ sicp-1.12.py
@@ -1,15 +1,11 @@
 # Pascal's triangle
 
 def pascal(n):
-    result = [1]
     if n == 1:
-        return result
+        return [1]
     else:
-        prev = pascal(n-1)
-        for i in range(0, len(prev) - 1):
-            result.append(prev[i] + prev[i+1])
-        result.append(1)
-        return result
+        prev = pascal(n - 1)
+        return [1] + [prev[i] + prev[i+1] for i in range(0, n - 2)] + [1]
 
 for i in range(5):
     print pascal(i+1)
